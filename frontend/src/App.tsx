f829fed33dcc069c77ec89ef13260b6eea7a5fd7
Renan <renancossenzo@hotmail.com>
2025-05-02 12:34:38 -0300

    Create components and instal bootstrap

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Input from './components/Input/Input';
+import Result from './components/Result/Result';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="app">
+            <Input />
+            <Result />
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/Input/Input.tsx
@@ -0,0 +1,5 @@
+const Input = () => {
+    return <h1>Input</h1>
+}
+
+export default Input;
