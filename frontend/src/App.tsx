bc97daeb6e12b55c15d32a971dd6a8a9d2455697
Renan <renancossenzo@hotmail.com>
2025-05-02 21:31:10 -0300

    Create title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,10 @@ import Result from './components/Result/Result';
 
 const App = () => {
     return (
-        <main className="app justify-content-center vh-100 d-flex w-100 h-100 p-3 mx-auto flex-column">
+        <main className="app justify-content-center vh-100 d-flex w-100 h-100 mx-auto flex-column">
+            <div className="app-title text-center">
+                <h1 className="display-3">Url Shortener</h1>
+            </div>
             <Input />
             <Result />
         </main>
