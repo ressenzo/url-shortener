6c73326a3dbfb5ea25084fba5d5fc0bc002d26cb
Renan <renancossenzo@hotmail.com>
2025-05-03 00:12:01 -0300

    Add UrlMapper

---
+++ backend/src/UrlShortener.Infrastructure/Mappers/UrlMapper.cs
@@ -0,0 +1,17 @@
+using UrlShortener.Domain.Entities;
+
+namespace UrlShortener.Infrastructure.Mappers;
+
+internal class UrlMapper
+{
+    public string Id { get; init; } = default!;
+    
+    public string OriginalUrl { get; init; } = default!;
+
+    public DateTime CreatedAt { get; init; }
+
+    public Url ToEntity() =>
+        Url.Factory(
+            Id,
+            OriginalUrl);
+}
