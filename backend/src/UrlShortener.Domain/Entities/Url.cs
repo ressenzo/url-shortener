fad883f235388ccbc949601bce264874da55a03b
Renan <renancossenzo@hotmail.com>
2025-05-02 23:46:35 -0300

    Implement and test Url

---
+++ backend/src/UrlShortener.Domain/Entities/Url.cs
@@ -0,0 +1,45 @@
+namespace UrlShortener.Domain.Entities;
+
+public class Url : Entity
+{
+    private Url(
+        string id,
+        string orignialUrl) : base(id)
+    {
+        OriginalUrl = orignialUrl;
+    }
+
+    private Url(
+        string orignialUrl) : base(id: null)
+    {
+        OriginalUrl = orignialUrl;
+    }
+
+    public string OriginalUrl { get; }
+
+    public static Url Factory(
+        string id,
+        string originalUrl) =>
+        new(id, originalUrl);
+
+    public static Url Factory(
+        string originalUrl) =>
+        new(originalUrl);
+
+    public override bool IsValid()
+    {
+        if (string.IsNullOrWhiteSpace(OriginalUrl))
+        {
+            AddErrors(["Original Url was not provided"]);
+            return false;
+        }
+
+        if (!Uri.IsWellFormedUriString(OriginalUrl, UriKind.RelativeOrAbsolute))
+        {
+            AddErrors(["Provided Original Url is not valid"]);
+            return false;
+        }
+        
+        return true;
+    }
+}
