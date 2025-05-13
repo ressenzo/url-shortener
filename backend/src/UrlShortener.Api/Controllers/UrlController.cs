5954ed996fa12620ef04ff2f69f202c0f4d9aa03
Renan <renancossenzo@hotmail.com>
2025-05-12 22:19:34 -0300

    Add return status code according to result.Status

--- backend/src/UrlShortener.Api/Controllers/UrlController.cs
+++ backend/src/UrlShortener.Api/Controllers/UrlController.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using UrlShortener.Api.Requests;
 using UrlShortener.Application.Shared;
+using UrlShortener.Application.UseCases.GetUrl;
 using UrlShortener.Application.UseCases.ShortenUrl;
 
 namespace UrlShortener.Api.Controllers;
@@ -10,7 +12,8 @@ namespace UrlShortener.Api.Controllers;
 [ApiController]
 [Route("api/urls")]
 public class UrlController(
-    IShortenUrlUseCase shortenUrlUseCase) : ControllerBase
+    IShortenUrlUseCase shortenUrlUseCase,
+    IGetUrlUseCase getUrlUseCase) : ControllerBase
 {
     [HttpPost]
     [SwaggerResponse(statusCode: (int)HttpStatusCode.Created, type: typeof(ShortenUrlResponse))]
@@ -32,6 +35,27 @@ public class UrlController(
         {
             ResultStatus.Success => Created("", result.Content),
             ResultStatus.ValidationError => BadRequest(result),
+            _ => throw new NotImplementedException()
+        };
+    }
+
+    [HttpGet("{id}")]
+    [SwaggerResponse(statusCode: (int)HttpStatusCode.OK, type: typeof(ShortenUrlResponse))]
+    [SwaggerResponse(statusCode: (int)HttpStatusCode.BadRequest, type: typeof(BaseResult))]
+    [SwaggerResponse(statusCode: (int)HttpStatusCode.NotFound, type: typeof(BaseResult))]
+    [SwaggerResponse(statusCode: (int)HttpStatusCode.InternalServerError, type: typeof(Result))]
+    public async Task<IActionResult> GetUrl(
+        [FromRoute][Required] string id,
+        CancellationToken cancellationToken)
+    {
+        var result = await getUrlUseCase.GetUrl(
+            id,
+            cancellationToken);
+        return result.Status switch
+        {
+            ResultStatus.Success => Redirect(result.Content!.OriginalUrl),
+            ResultStatus.ValidationError => BadRequest(result),
+            ResultStatus.NotFound => NotFound(result),
             _ => throw new NotImplementedException()
         };
     }
