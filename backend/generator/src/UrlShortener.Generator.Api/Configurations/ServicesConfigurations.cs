4c3ce936c369214e7bab5badbebae3afe6497e60
Renan <renancossenzo@hotmail.com>
2025-12-20 19:22:21 -0300

    Fix health check method name

--- backend/generator/src/UrlShortener.Generator.Api/Configurations/ServicesConfigurations.cs
+++ backend/generator/src/UrlShortener.Generator.Api/Configurations/ServicesConfigurations.cs
@@ -17,7 +17,7 @@ internal static class ServicesConfigurations
 	{
 		services
 			.AddLayers(configuration)
-			.AddHealthChecks()
+			.AddHealthCheck()
 			.AddSettings(configuration)
 			.AddFilters()
 			.AddCors()
@@ -39,7 +39,7 @@ internal static class ServicesConfigurations
 		return services;
 	}
 
-	private static IServiceCollection AddHealthChecks(
+	private static IServiceCollection AddHealthCheck(
 		this IServiceCollection services
 	)
 	{
