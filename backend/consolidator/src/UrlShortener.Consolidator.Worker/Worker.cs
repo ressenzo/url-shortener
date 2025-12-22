fdd7ce1a6b2528906f0e8d83fba89a05232d0cd1
Renan <renancossenzo@hotmail.com>
2025-12-21 21:08:44 -0300

    Call messaging repo

--- backend/consolidator/src/UrlShortener.Consolidator.Worker/Worker.cs
+++ backend/consolidator/src/UrlShortener.Consolidator.Worker/Worker.cs
@@ -1,49 +1,19 @@
-using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
+using UrlShortener.Consolidator.Application.Repositories;
 
 namespace UrlShortener.Consolidator.Worker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(
+	ILogger<Worker> logger,
+	IMessagingConsumerRepository messagingRepository) : BackgroundService
 {
 	// 3 minutes
 	private const int _DELAY_IN_MILISECONDS = 180000;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-		var factory = new ConnectionFactory()
-		{
-			HostName = "localhost",
-			Port = 5672,
-			VirtualHost = "/",
-			UserName = "guest",
-			Password = "guest"
-		};
-		using var connection = await factory.CreateConnectionAsync(
-			stoppingToken
-		);
-		using var channel = await connection.CreateChannelAsync(
-			cancellationToken: stoppingToken
-		);
-		await CreateQueue(channel);
-		var consumer = new AsyncEventingBasicConsumer(
-			channel
-		);
-		consumer.ReceivedAsync += async (model, ea) =>
-		{
-			var body = ea.Body.ToArray();
-			var message = Encoding.UTF8.GetString(body);
-			Console.WriteLine(message);
-		};
+        await messagingRepository.Start(stoppingToken);
 
-		await channel.BasicConsumeAsync(
-			queue: "url-shortener.creation",
-			autoAck: false,
-			consumer,
-			stoppingToken
-		);
-
-        while (!stoppingToken.IsCancellationRequested)
+		while (!stoppingToken.IsCancellationRequested)
         {
             if (logger.IsEnabled(LogLevel.Information))
             {
@@ -52,15 +22,4 @@ public class Worker(ILogger<Worker> logger) : BackgroundService
             await Task.Delay(_DELAY_IN_MILISECONDS, stoppingToken);
         }
     }
-
-	private static async Task CreateQueue(IChannel channel)
-	{
-		await channel.QueueDeclareAsync(
-			queue: "url-shortener.creation",
-			durable: false,
-			exclusive: false,
-			autoDelete: false,
-			arguments: null
-		);
-	}
 }
