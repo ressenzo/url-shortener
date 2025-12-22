17a1e8e2651d7b15b4db591a86946e08306ad084
Renan <renancossenzo@hotmail.com>
2025-12-21 21:08:26 -0300

    Add layers

--- backend/consolidator/src/UrlShortener.Consolidator.Worker/Program.cs
+++ backend/consolidator/src/UrlShortener.Consolidator.Worker/Program.cs
@@ -1,6 +1,10 @@
 using UrlShortener.Consolidator.Worker;
+using UrlShortener.Consolidator.Infrastructure;
+using UrlShortener.Consolidator.Application;
 
 var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+builder.Services.AddApplicationLayer();
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
