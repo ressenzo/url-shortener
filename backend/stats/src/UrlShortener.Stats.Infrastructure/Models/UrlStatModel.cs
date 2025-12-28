2bb556b97bf6e230a476c15679736fbbd9df3caf
Renan <renancossenzo@hotmail.com>
2025-12-28 20:51:48 -0300

    feat: add database model

---
+++ backend/stats/src/UrlShortener.Stats.Infrastructure/Models/UrlStatModel.cs
@@ -0,0 +1,36 @@
+using UrlShortener.Stats.Domain.Entities;
+using UrlShortener.Stats.Domain.Entities.Interfaces;
+
+namespace UrlShortener.Stats.Infrastructure.Models;
+
+public class UrlStatModel(
+	string id,
+	string originalUrl,
+	int accessesQuantity,
+	DateTime lastAccess
+)
+{
+	public string Id { get; private set; } = id;
+
+	public string OriginalUrl { get; private set; } = originalUrl;
+
+	public int AccessesQuantity { get; private set; } = accessesQuantity;
+
+	public DateTime LastAccess { get; private set; } = lastAccess;
+
+	public static UrlStatModel FromEntity(IUrlStat urlStat) =>
+		new(
+			urlStat.Id,
+			urlStat.OriginalUrl,
+			urlStat.AccessesQuantity,
+			urlStat.LastAccess
+		);
+
+	public IUrlStat ToEntity() =>
+		new UrlStat(
+			Id,
+			OriginalUrl,
+			AccessesQuantity,
+			LastAccess
+		);
+}
