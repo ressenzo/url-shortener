a6a297b2e1400600d5cd7caf9b8f073248fa6fb4
Renan <renancossenzo@hotmail.com>
2026-01-11 20:44:06 -0300

    feat: implement mongo db transactions using mongo

--- backend/stats/internal/application/url_stat_repository.go
+++ backend/stats/internal/application/url_stat_repository.go
@@ -3,6 +3,6 @@ package application
 import "url-shortener/stats/internal/domain"
 
 type UrlStatRepository interface {
-	SaveUrlStat(urlStat domain.UrlStat)
-	GetUrlStat(id string) *domain.UrlStat
+	SaveUrlStat(urlStat domain.UrlStat) (domain.UrlStat, error)
+	GetUrlStat(id string) (*domain.UrlStat, error)
 }
