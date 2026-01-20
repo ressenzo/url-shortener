4211c1a8482afad36a3eba3b708bef97aca46af5
Renan <renancossenzo@hotmail.com>
2026-01-19 21:42:40 -0300

    fix: recreate docker configs to go app

--- backend/stats/cmd/config/db_config.go
+++ backend/stats/cmd/config/db_config.go
@@ -3,15 +3,21 @@ package config
 import (
 	"context"
 	"log"
+	"os"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func DbConfig() *mongo.Database {
+	mongoUri := os.Getenv("MONGODB_URI")
+	if mongoUri == "" {
+		mongoUri = "mongodb://admin:admin123@localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+	}
+
 	client, err := mongo.Connect(
 		context.Background(),
-		options.Client().ApplyURI("mongodb://admin:admin123@localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000"),
+		options.Client().ApplyURI(mongoUri),
 	)
 
 	if err != nil {
